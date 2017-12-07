030550ef24a9cc6fe4090d81c44038e47e000568
Martin Tournoij <martin@arp242.net>
2017-12-07 08:32:32 +0000

    Support multierror

--- doc.go
+++ doc.go
@@ -1,11 +1,17 @@
 // Package try provides retry functionality.
 //     var value string
-//     err := try.Do(func(attempt int) (bool, error) {
+//     err := retry.Do(func(attempt int) (retry bool, err error) {
 //       var err error
 //       value, err = SomeFunction()
 //       return attempt < 5, err // try 5 times
 //     })
 //     if err != nil {
-//       log.Fatalln("error:", err)
+//       log.Error(err, "somefunction failed")
 //     }
+//
+// This package was created by Mat Ryer and modified to support multierr, the
+// original package is available at:
+//   https://github.com/matryer/try
+// With the post introducing it at:
+//   https://medium.com/@matryer/retrying-in-golang-quicktip-f688d00e650a#.3r2nbnjwu
 package try
--- try_test.go
+++ try_test.go
@@ -1,101 +1,136 @@
-package try_test
+package try
 
 import (
 	"errors"
 	"fmt"
-	"log"
+	"reflect"
 	"testing"
 
-	"github.com/cheekybits/is"
-	"github.com/matryer/try"
+	multierror "github.com/hashicorp/go-multierror"
 )
 
 func TestTryExample(t *testing.T) {
-	try.MaxRetries = 20
 	SomeFunction := func() (string, error) {
 		return "", nil
 	}
-	var value string
-	err := try.Do(func(attempt int) (bool, error) {
+	err := Do(func(attempt int) (bool, error) {
 		var err error
-		value, err = SomeFunction()
+		_, err = SomeFunction()
 		return attempt < 5, err // try 5 times
 	})
 	if err != nil {
-		log.Fatalln("error:", err)
-	}
-}
-
-func TestTryExamplePanic(t *testing.T) {
-	SomeFunction := func() (string, error) {
-		panic("something went badly wrong")
-	}
-	var value string
-	err := try.Do(func(attempt int) (retry bool, err error) {
-		retry = attempt < 5 // try 5 times
-		defer func() {
-			if r := recover(); r != nil {
-				err = errors.New(fmt.Sprintf("panic: %v", r))
-			}
-		}()
-		value, err = SomeFunction()
-		return
-	})
-	if err != nil {
-		//log.Fatalln("error:", err)
+		t.Error(err)
 	}
 }
 
 func TestTryDoSuccessful(t *testing.T) {
-	is := is.New(t)
 	callCount := 0
-	err := try.Do(func(attempt int) (bool, error) {
+	err := Do(func(attempt int) (bool, error) {
 		callCount++
 		return attempt < 5, nil
 	})
-	is.NoErr(err)
-	is.Equal(callCount, 1)
+
+	if err != nil {
+		t.Errorf("unexpected err: %v", err)
+	}
+	if callCount != 1 {
+		t.Errorf("expected callCount: %d, got: %d", 1, callCount)
+	}
+}
+
+func TestTryDoSuccessfulAfterFailure(t *testing.T) {
+	callCount := 0
+	err := Do(func(attempt int) (bool, error) {
+		callCount++
+
+		if attempt < 3 {
+			return true, fmt.Errorf("failure %d", attempt)
+		}
+		return false, nil
+	})
+
+	if err != nil {
+		t.Errorf("unexpected err: %v", err)
+	}
+	if callCount != 3 {
+		t.Errorf("expected callCount: %d, got: %d", 3, callCount)
+	}
 }
 
 func TestTryDoFailed(t *testing.T) {
-	is := is.New(t)
-	theErr := errors.New("something went wrong")
+	var errs error
+	for i := 1; i <= 5; i++ {
+		errs = multierror.Append(errs, fmt.Errorf("err attempt: %d", i))
+	}
 	callCount := 0
-	err := try.Do(func(attempt int) (bool, error) {
+	err := Do(func(attempt int) (bool, error) {
 		callCount++
-		return attempt < 5, theErr
+		return attempt < 5, fmt.Errorf("err attempt: %d", attempt)
 	})
-	is.Equal(err, theErr)
-	is.Equal(callCount, 5)
+
+	if !reflect.DeepEqual(err, errs) {
+		t.Errorf("expected err: \n%v\ngot:\n%v", errs, err)
+	}
+	if callCount != 5 {
+		t.Errorf("expected callCount: %d, got: %d", 5, callCount)
+	}
 }
 
 func TestTryPanics(t *testing.T) {
-	is := is.New(t)
 	theErr := errors.New("something went wrong")
+	errs := multierror.Append(theErr, errors.New("panic: I don't like three"))
 	callCount := 0
-	err := try.Do(func(attempt int) (retry bool, err error) {
-		retry = attempt < 5
+	err := Do(func(attempt int) (retry bool, err error) {
+		retry = attempt < 2
 		defer func() {
 			if r := recover(); r != nil {
-				err = errors.New(fmt.Sprintf("panic: %v", r))
+				err = fmt.Errorf("panic: %v", r)
 			}
 		}()
 		callCount++
-		if attempt > 2 {
+		if attempt == 2 {
 			panic("I don't like three")
 		}
 		err = theErr
 		return
 	})
-	is.Equal(err.Error(), "panic: I don't like three")
-	is.Equal(callCount, 5)
+
+	if !reflect.DeepEqual(err, errs) {
+		t.Errorf("expected err: \n%v\ngot:\n%v", errs, err)
+	}
+	if callCount != 2 {
+		t.Errorf("expected callCount: %d, got: %d", 5, callCount)
+	}
 }
 
 func TestRetryLimit(t *testing.T) {
-	is := is.New(t)
-	err := try.Do(func(attempt int) (bool, error) {
+	err := Do(func(attempt int) (bool, error) {
 		return true, errors.New("nope")
 	})
-	is.OK(err)
-	is.Equal(try.IsMaxRetries(err), true)
+
+	if err == nil {
+		t.Errorf("expected err, got: <nil>")
+	}
+	if !IsMaxRetries(err) {
+		t.Errorf("expected IsMaxRetries to be true")
+	}
+}
+
+func TestIsMaxRetries(t *testing.T) {
+	tests := []struct {
+		in  error
+		out bool
+	}{
+		{nil, false},
+		{errors.New("nope"), false},
+		{errMaxRetriesReached, true},
+		{multierror.Append(nil), false},
+		{multierror.Append(nil, errMaxRetriesReached), true},
+		{multierror.Append(errors.New("something"), errMaxRetriesReached), true},
+	}
+	for _, tt := range tests {
+		if IsMaxRetries(tt.in) != tt.out {
+			t.Errorf("isMaxRetries(%v) expected to be %t", tt.in, tt.out)
+		}
+	}
 }
